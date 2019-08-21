7a276b9fe4611570b9270260609ba961ca63d971
小俣圭佑 <omatakeisuke@MacBook.local>
2019-08-21 19:39:06 +0900

    修正

--- db/migrate/20190804085726_create_spots.rb
+++ db/migrate/20190804085726_create_spots.rb
@@ -1,6 +1,6 @@
 class CreateSpots < ActiveRecord::Migration[5.2]
   def change
-    create_table :spots do |t|
+    create_table :spots, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :user_id
       t.string :spot_name
       t.integer :category
--- db/migrate/20190808055646_create_livehouses.rb
+++ db/migrate/20190808055646_create_livehouses.rb
@@ -1,6 +1,6 @@
 class CreateLivehouses < ActiveRecord::Migration[5.2]
   def change
-    create_table :livehouses do |t|
+    create_table :livehouses, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.string :livehouse_name
       t.string :address
       t.float :latitude
