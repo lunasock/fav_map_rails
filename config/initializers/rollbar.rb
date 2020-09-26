bec83ec198358b4880bf1736c55be504118ed6d0
小俣圭佑 <omatakeisuke@MacBook.local>
2020-09-26 18:56:19 +0900

    [edit] rollbarのアクセストークンの持ち方を変更

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,8 +2,8 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-
+  config.access_token = Rails.application.credentials.rollbar[:ROLLBAR_ACCESS_TOKEN]
+  
   # Here we'll disable in 'test':
   if Rails.env.test?
     config.enabled = false
