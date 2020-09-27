f2a174826126fbbad1c4220c4a07b2a13c4dee6a
小俣圭佑 <omatakeisuke@MacBook.local>
2020-09-27 10:49:11 +0900

    [edit] rollbarとomniauthの設定を変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # 開発環境と本番環境で分ける場合
-  if Rails.env.development? || Rails.env.test?
-    provider :github,
-      Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret],
+  if Rails.env.test?
+    provider :github, 1, 1,
       # localhostからgithub認証ができない場合
       {:provider_ignores_state => true}
   else
     provider :github,
       Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret]
+      Rails.application.credentials.github[:client_secret],
+      # localhostからgithub認証ができない場合
+      {:provider_ignores_state => true}
   end
 end
